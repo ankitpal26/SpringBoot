efb66aa3c14d7bfa3c50d1f1c7e290c2f942c7d0
Ankit <ankitpal28090016@gmail.com>
2023-08-04 18:30:16 +0530

    One To One Mapping with springboot

---
+++ bootrestbook/src/main/java/com/test/bootrestbook/entities/Author.java
@@ -0,0 +1,58 @@
+package com.test.bootrestbook.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int authorId;
+    private String lastName;
+    private String firstName;
+    private String language;
+
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Author() {
+    }
+
+    public Author(int authorId, String lastName, String firstName, String language) {
+        this.authorId = authorId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.language = language;
+    }
+
+}
--- bootrestbook/src/main/java/com/test/bootrestbook/entities/Book.java
+++ bootrestbook/src/main/java/com/test/bootrestbook/entities/Book.java
@@ -1,9 +1,5 @@
 package com.test.bootrestbook.entities;
-
-
-
 import javax.persistence.*;
-
 
 @Entity
 @Table(name = "books")
@@ -15,9 +11,11 @@ public class Book {
     @Column(name = "book_id")
     private int id;
     private String title;
-    private  String author;
 
-    public Book(int id, String title, String author) {
+    @OneToOne(cascade = CascadeType.ALL)
+    private  Author author;
+
+    public Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -43,11 +41,11 @@ public class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
