b9208ffe9704818836923735b861f24f7f724c44
Ankit <ankitpal28090016@gmail.com>
2023-08-06 15:46:23 +0530

    file uploading in database

--- bootrestbook/src/main/java/com/test/bootrestbook/controllers/FileUploadController.java
+++ bootrestbook/src/main/java/com/test/bootrestbook/controllers/FileUploadController.java
@@ -1,5 +1,7 @@
 package com.test.bootrestbook.controllers;
 
+import com.test.bootrestbook.helper.FileUploadHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
     @PostMapping("/upload-file")
     public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file)
     {
@@ -17,19 +22,25 @@ public class FileUploadController {
 //        System.out.println(file.getSize());
 //        System.out.println(file.getContentType());
 //        System.out.println(file.getName());
-        if(file.isEmpty())
-        {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request must contain file");
+        try {
+            //validation
+            if (file.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request must contain file");
+            }
+            if (file.getContentType().equals("image/jpeg")) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only jpeg content type are allowed");
+            }
+
+            //file upload code...
+        boolean f=   fileUploadHelper.uploadFile(file);
+            if(f){
+                return ResponseEntity.ok("File is successfully uploaded");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        if(file.getContentType().equals("image/jpeg"))
-        {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Only jpeg content type are allowed");
-        }
-
-        //file upload code...
-
-
-        return ResponseEntity.ok("working");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong ! Try again ");
     }
 
 }
--- bootrestbook/src/main/java/com/test/bootrestbook/helper/FileUploadHelper.java
+++ bootrestbook/src/main/java/com/test/bootrestbook/helper/FileUploadHelper.java
@@ -3,7 +3,13 @@ package com.test.bootrestbook.helper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Component
 public class FileUploadHelper {
@@ -16,9 +22,24 @@ public class FileUploadHelper {
 
         try {
 
-            InputStream is = multipart.getInputStream();
-            byte
+//            InputStream is = multipart.getInputStream();
+//            byte data[] = new byte[is.available()];
+//            is.read(data);
+//
+//            //Write
+//            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator+multipart.getOriginalFilename());
+//            fos.write(data);
+//
+//            fos.flush();;
+//            fos.close();
 
+            Files.copy(multipart.getInputStream(), Paths.get(UPLOAD_DIR +File.separator+multipart.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+
+
+
+
+
+            f=true;
 
         }catch (Exception e){
 
