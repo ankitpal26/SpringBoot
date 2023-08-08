2661b7b4f3f9ad28eb899599c688677a577d4d9e
Ankit <ankitpal28090016@gmail.com>
2023-08-08 23:49:44 +0530

    Starting thymeleaf

---
+++ thymeleafPractical/src/main/java/com/practice/thymeleafPractical/controllers/MyController.java
@@ -0,0 +1,26 @@
+package com.practice.thymeleafPractical.controllers;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+public class MyController {
+
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
+    public String about(Model model)
+    {
+        System.out.println("Inside about handler....");
+
+        //Putting data in model
+        model.addAttribute("name","Ankit Pal");
+
+        model.addAttribute("currentDate",new Date().toLocaleString());
+        return "about";
+        //about.html
+    }
+}
