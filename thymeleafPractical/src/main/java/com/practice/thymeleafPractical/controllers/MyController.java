aea4b2f064f7df7970a6954442e58e40cf19bb47
Ankit <ankitpal28090016@gmail.com>
2023-08-30 13:17:28 +0530

    create a api example-loop for send data in iterate.html

--- thymeleafPractical/src/main/java/com/practice/thymeleafPractical/controllers/MyController.java
+++ thymeleafPractical/src/main/java/com/practice/thymeleafPractical/controllers/MyController.java
@@ -2,10 +2,12 @@ package com.practice.thymeleafPractical.controllers;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -23,9 +25,19 @@ public class MyController {
         //example
         String nameString="abc";
         nameString.toUpperCase();
-
-
         return "about";
         //about.html
+    }
+
+    @GetMapping("/example-loop")
+    public String iteraterHandler(Model m){
+        //send
+
+        //Creating a list , set collection
+        List<String> names = List.of("Ankit", "Laxmi", "Karan", "John");
+        m.addAttribute("names",names);
+
+
+        return "iterate";
     }
 }
