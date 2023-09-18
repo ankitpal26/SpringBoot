7d7ef6819316e0a4a51e0f25e4503abfc434dee3
Ankit <ankitpal28090016@gmail.com>
2023-09-18 19:17:31 +0530

    Configured UserDetailsService through @Bean class

--- Jwt-Token/src/main/java/com/jwt/example/controller/HomeController.java
+++ Jwt-Token/src/main/java/com/jwt/example/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,10 @@ public class HomeController {
         System.out.println("getting users");
 
         return this.userService.getUsers();
+    }
+
+    @GetMapping("/current-user")
+    public String getLoggedInUser(Principal principal) {
+        return principal.getName();
     }
 }
